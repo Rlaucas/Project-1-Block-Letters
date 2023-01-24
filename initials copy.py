b8627e14b09226fc5309a04b23334795a61168a9
Renato Laucas <70399246+Rlaucas@users.noreply.github.com>
2023-01-24 13:48:37 -0500

    first commit

---
+++ initials copy.py
@@ -0,0 +1,27 @@
+# This program prints ASCII art with my initials (RL) and it will be sent to GitHub
+R = ("R")
+L = ("L")
+
+Horizontal_one = 6 * R + 2 * " " + L
+Horizontal_two = R + 4 * " " + R + 2 * " "+ L
+Horizontal_three = R + 4 * " " + R + 2 * " " + L
+Horizontal_four = R + 4 * " " + R + 2 * " " + L
+Horizontal_five = 6 * R + 2 * " " + L
+Horizontal_six = 2 * R + 6 * " " + L
+Horizontal_seven = R + " " + R + 5 * " " + L
+Horizontal_eight = R + 2 * " " + R + 4 * " " + L
+Horizontal_nine = R + 3 * " " + R + 4 * " " + L
+Horizontal_ten = R + 3 * " " + R + 3 * " " + L
+Horizontal_eleven = R + 4 * " " + R + 2 * " " + 6 * L
+
+print(Horizontal_one)
+print(Horizontal_two)
+print(Horizontal_three)
+print(Horizontal_four)
+print(Horizontal_five)
+print(Horizontal_six)
+print(Horizontal_seven)
+print(Horizontal_eight)
+print(Horizontal_ten)
+print(Horizontal_eleven)
+
